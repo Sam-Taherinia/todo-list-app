0981eb6b035473d2a030faa9e6307a2ff60987e2
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-05 19:52:00 +0330

    added update task-list feature

--- backend/src/main/java/com/tromaya/todo_list/services/impl/TaskListServiceImpl.java
+++ backend/src/main/java/com/tromaya/todo_list/services/impl/TaskListServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -46,5 +47,23 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public Optional<TaskList> getTaskList(UUID id) {
         return taskListRepository.findById(id);
+    }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if (null == taskList.getId()) {
+            throw new IllegalArgumentException("Task-list ID is required!");
+        }
+        if (!Objects.equals(taskList.getId(), taskListId)) {
+            throw new IllegalArgumentException("Task-list ID does not match, this is not permitted!");
+        }
+        TaskList existingTaskList = taskListRepository.findById(taskListId).orElseThrow(() ->
+                new IllegalArgumentException("Task-list ID does not exist!")
+        );
+
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdatedDate(LocalDateTime.now());
+        return taskListRepository.save(existingTaskList);
     }
 }
