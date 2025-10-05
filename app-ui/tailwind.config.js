1afc20f31505ff789d6f8ed45319bccaf8504754
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-05 23:47:14 +0330

    updated tailwind config. changed the project theme and primary color

--- app-ui/tailwind.config.js
+++ app-ui/tailwind.config.js
@@ -5,51 +5,52 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui({
-    themes: {
-      dark: {
-        colors: {
-          primary: {
-            50: "#fffbeb",
-            100: "#fef3c7",
-            200: "#fde68a",
-            300: "#fcd34d",
-            400: "#fbbf24",
-            500: "#FFB703", // Your custom color
-            600: "#d97706",
-            700: "#b45309",
-            800: "#92400e",
-            900: "#78350f",
-            DEFAULT: "#FFB703",
-            foreground: "#000000"
-          }
-        }
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          colors: {
+            primary: {
+              50: "#fffbeb",
+              100: "#fef3c7",
+              200: "#fde68a",
+              300: "#fcd34d",
+              400: "#fbbf24",
+              500: "#FFB703", // main color
+              600: "#d97706",
+              700: "#b45309",
+              800: "#92400e",
+              900: "#78350f",
+              DEFAULT: "#FFB703",
+              foreground: "#000000",
+            },
+          },
+        },
+        light: {
+          colors: {
+            primary: {
+              50: "#fffbeb",
+              100: "#fef3c7",
+              200: "#fde68a",
+              300: "#fcd34d",
+              400: "#fbbf24",
+              500: "#FFB703", // main color
+              600: "#d97706",
+              700: "#b45309",
+              800: "#92400e",
+              900: "#78350f",
+              DEFAULT: "#FFB703",
+              foreground: "#000000",
+            },
+          },
+        },
       },
-      light: {
-        colors: {
-          primary: {
-            50: "#fffbeb",
-            100: "#fef3c7",
-            200: "#fde68a",
-            300: "#fcd34d",
-            400: "#fbbf24",
-            500: "#FFB703", // Your custom color
-            600: "#d97706",
-            700: "#b45309",
-            800: "#92400e",
-            900: "#78350f",
-            DEFAULT: "#FFB703",
-            foreground: "#000000"
-          }
-        }
-      }
-    }
-  })]
-}
-
+    }),
+  ],
+};
