2c0617c6c5bd0faabdb5ceed04344b9695cd5229
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-05 15:07:49 +0330

    updated tasklist controller, service and service impl

--- backend/src/main/java/com/tromaya/todo_list/services/TaskListService.java
+++ backend/src/main/java/com/tromaya/todo_list/services/TaskListService.java
@@ -3,9 +3,11 @@ package com.tromaya.todo_list.services;
 import com.tromaya.todo_list.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
-
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
