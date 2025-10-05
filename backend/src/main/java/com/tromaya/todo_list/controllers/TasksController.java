dc0cd905350cc3b1ac4219f9900b3747656b9d7b
Sam-TN <meysamtaherinia76@gmail.com>
2025-10-06 00:09:32 +0330

    updated task controller, service and service impl

--- backend/src/main/java/com/tromaya/todo_list/controllers/TasksController.java
+++ backend/src/main/java/com/tromaya/todo_list/controllers/TasksController.java
@@ -1,12 +1,10 @@
 package com.tromaya.todo_list.controllers;
 
 import com.tromaya.todo_list.domain.dto.TaskDto;
+import com.tromaya.todo_list.domain.entities.Task;
 import com.tromaya.todo_list.mappers.TaskMapper;
 import com.tromaya.todo_list.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -30,4 +28,17 @@ public class TasksController {
                 .map(taskMapper::toDto)
                 .toList();
     }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto
+    ){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(createdTask);
+    }
+
 }
